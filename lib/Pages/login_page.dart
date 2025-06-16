289f2727f3acddb9bed8de9e7aa127b7f0a477d5
AbdulazizAlWadhaf <abdulazizalwadhaf123@gmail.com>
2025-06-16 14:17:51 -0700

    make the pages scrollable

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -13,53 +13,58 @@ class LoginPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.message_rounded, size: 196),
-              const SizedBox(height: 64),
-              CustomTextField(hintText: 'Email'),
-              const SizedBox(height: 16),
-              CustomTextField(hintText: 'Password'),
-              const SizedBox(height: 32),
-              IconButton(
-                onPressed: () {},
-                icon: Container(
-                  height: 64,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: Colors.blue,
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Log In',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.message_rounded, size: 196),
+                const SizedBox(height: 64),
+                CustomTextField(hintText: 'Email'),
+                const SizedBox(height: 16),
+                CustomTextField(hintText: 'Password'),
+                const SizedBox(height: 32),
+                IconButton(
+                  onPressed: () {},
+                  icon: Container(
+                    height: 64,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.blue,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Log In',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 6),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Don\'t have an account? ',
-                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
-                  ),
-                  CustomGestureDetector(
-                    text: ' Sign Up',
-                    onTap: () {
-                      Navigator.pushNamed(context, SignupPage.id);
-                    },
-                  ),
-                ],
-              ),
-            ],
+                const SizedBox(height: 6),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Don\'t have an account? ',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 18,
+                      ),
+                    ),
+                    CustomGestureDetector(
+                      text: ' Sign Up',
+                      onTap: () {
+                        Navigator.pushNamed(context, SignupPage.id);
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- lib/Pages/signup_page.dart
+++ lib/Pages/signup_page.dart
@@ -13,53 +13,58 @@ class SignupPage extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(16),
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.message_rounded, size: 196),
-              const SizedBox(height: 64),
-              CustomTextField(hintText: 'Email'),
-              const SizedBox(height: 16),
-              CustomTextField(hintText: 'Password'),
-              const SizedBox(height: 32),
-              IconButton(
-                onPressed: () {},
-                icon: Container(
-                  height: 64,
-                  width: double.infinity,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: Colors.blue,
-                  ),
-                  child: Center(
-                    child: Text(
-                      'Sign Up',
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Icon(Icons.message_rounded, size: 196),
+                const SizedBox(height: 64),
+                CustomTextField(hintText: 'Email'),
+                const SizedBox(height: 16),
+                CustomTextField(hintText: 'Password'),
+                const SizedBox(height: 32),
+                IconButton(
+                  onPressed: () {},
+                  icon: Container(
+                    height: 64,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(16),
+                      color: Colors.blue,
+                    ),
+                    child: Center(
+                      child: Text(
+                        'Sign Up',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18,
+                        ),
                       ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(height: 6),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    'Already have an account?',
-                    style: TextStyle(fontWeight: FontWeight.w400, fontSize: 18),
-                  ),
-                  CustomGestureDetector(
-                    text: ' Log in',
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-            ],
+                const SizedBox(height: 6),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Already have an account?',
+                      style: TextStyle(
+                        fontWeight: FontWeight.w400,
+                        fontSize: 18,
+                      ),
+                    ),
+                    CustomGestureDetector(
+                      text: ' Log in',
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
