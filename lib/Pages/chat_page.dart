059113acafc8c53c48fbc82692478c00929033e0
AbdulazizAlWadhaf <abdulazizalwadhaf123@gmail.com>
2025-06-22 14:13:37 -0700

    chat page

--- lib/Pages/chat_page.dart
+++ lib/Pages/chat_page.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 import '../widgets/chat_bubble.dart';
 
 class ChatPage extends StatelessWidget {
-  const ChatPage({super.key});
+  ChatPage({super.key});
 
   static String id = 'ChatPage';
+  CollectionReference messages = FirebaseFirestore.instance.collection(
+    'messages',
+  );
+  TextEditingController controller = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -33,6 +39,11 @@ class ChatPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.all(16),
             child: TextField(
+              controller: controller,
+              onSubmitted: (data) {
+                messages.add({'message': data});
+                controller.clear();
+              },
               decoration: InputDecoration(
                 hintText: 'Send a Message',
                 suffix: Icon(Icons.send, color: Colors.blue),
